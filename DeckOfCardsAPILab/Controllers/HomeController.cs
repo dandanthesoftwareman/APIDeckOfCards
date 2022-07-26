062686390e6c744aaa80b16a03ee7e086a3e8dad
Dan Schuler <104746354+dandanthesoftwareman@users.noreply.github.com>
2022-07-26 09:00:51 -0400

    First Commit, project incomplete

---
+++ DeckOfCardsAPILab/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using DeckOfCardsAPILab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net;
+
+namespace DeckOfCardsAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        DeckDAL deckDAL = new DeckDAL();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Draw()
+        {
+            DeckModel result = deckDAL.DrawCards();
+            {
+                if (result.remaining >= 1)
+                {
+                    return View(result);
+                }
+                else
+                {
+                    deckDAL.ShuffleCards();
+                    return View(result);
+                }
+            }
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCardsAPILab/Models/DeckDAL.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace DeckOfCardsAPILab.Models
+{
+    public class DeckDAL
+    {
+        public DeckModel DrawCards() //adjust
+        {
+            //adjust
+            //api url
+            string key = "vgln20rp1mwn";
+            string url = $"https://deckofcardsapi.com/api/deck/{key}/draw/?count=5";
+            //setup request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            //capture response
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            //save response data
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+            //adjust
+            //convert JSON into C# object
+            DeckModel result = JsonConvert.DeserializeObject<DeckModel>(JSON);
+            return result;
+        }
+        public void ShuffleCards()
+        {
+            string key = "vgln20rp1mwn";
+            string url = $"https://deckofcardsapi.com/api/deck/{key}/shuffle/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
+
